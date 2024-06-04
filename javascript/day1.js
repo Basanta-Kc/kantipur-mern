6d0d9d0f2a3fc86b07dff89fa4029f0db27e93f9
Basanta-Kc <devbasanta@gmail.com>
2024-06-04 16:39:28 +0545

    day 1 js (variable, type conversion, operators)

---
+++ javascript/day1.js
@@ -0,0 +1,74 @@
+// var a = 10;
+// a = true;
+// a = "basanta";
+// var b = 20;
+// let a = 10;
+// a = 20;
+// const GRAVITY = 9.8;
+// const name  = 'basnata';
+// GRAVITY = 9.5;
+
+// if(true) {
+//     let a = 10;
+
+//     var b = 20;
+//     console.log('hello world')
+// }
+
+// console.log(b)
+
+// function sayHello(){
+//     console.log(personName) // => udnefined
+//     var personName = 'basanta';
+//     console.log("hello world.")
+// }
+
+//   function sayHello() {
+//     var personName = "basanta";
+//     if (true) {
+//       var personAge = 10;
+//       let personSchool = "nbhs";
+//     }
+
+//     console.log(personAge); //
+//     console.log(personSchool);
+//   }
+
+//   sayHello()
+
+//  var personName;
+// console.log(perosnName)
+// perosnName = 'basanta'
+// sayHello()
+
+// console.log(personName)
+
+// let a = null;
+// console.log(a)
+//   const name = "basanta";
+//   const age = 10;
+//   const stringAge = "10";
+//   const fatherAge = 10;
+//   const favFruits = ["apple", "watermelon"];
+//   const hasPaidFee = false;
+//   let school; // undefined
+//   const college = null;
+//   const salary = 1234567890123456789012345678901234567890n;
+
+//   const persons = {
+//     name: "basanta",
+//     age: 10,
+//     school: "nbhs",
+//     salary: 10000,
+//   };
+
+//  console.log(age + +stringAge + fatherAge)
+
+let count = 0;
+let anotherCount = ++count;
+console.log(anotherCount);
+console.log(count);
+
+// falsy => Nan, undefined, null, "", 0
+// truthy => all other values
+console.log("a" > "A");
