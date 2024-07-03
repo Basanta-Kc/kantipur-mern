60221c1f7991ab92e6ebfdfab2eb30a4ca001c8a
Basanta-Kc <devbasanta@gmail.com>
2024-07-03 17:52:44 +0545

    todo crud dom

--- javascript/dom.js
+++ javascript/dom.js
@@ -23,3 +23,37 @@ button.addEventListener("click", (e) => {
 });
 
 const tasks = ["learn html", "learn css", "learn php"];
+tasks[1] = "learn java";
+tasks[2] = "learn php";
+
+// create, read, update, delete
+const deleteTask = (indexToBeDeleted) => {
+  tasks.splice(indexToBeDeleted, 1);
+};
+
+const updateTask = (index, updatedTask) => {
+  tasks[index] = updatedTask;
+  // task [0] = learn testing
+};
+
+const addTask = (newTask) => {
+  tasks.push(newTask);
+};
+
+const readTask = () => {
+  tasks.forEach((task) => {
+    console.log(task);
+  });
+};
+
+// readTask() => task list console
+// updateTask(index, value) => a[0] => learn php
+// deteTask(index)
+// addTask(value)
+addTask("learn java");
+addTask("learn laravel");
+readTask();
+deleteTask(4);
+readTask();
+updateTask(0, "learn testing");
+readTask();
