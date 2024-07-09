f578425274b117e62a9a8834bf874c15e78d9a5a
Basanta-Kc <devbasanta@gmail.com>
2024-07-09 16:47:32 +0545

    add and delete todo

--- node/index.js
+++ node/index.js
@@ -2,18 +2,46 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded());
 const todos = ["learn html", "learn css", "learn php"];
 
-app.get("/:id", (req, res) => {
-  console.log(req.query);
-  console.log(req.params);
+app.get("/", (req, res) => {
+  let taskList = "";
+  todos.forEach((todo, index) => {
+    taskList += `
+    <li>
+      ${todo} 
+      <form method="post" action="/delete/${index}">
+      <button type="submit">delete</button>
+      </form>
+    </li>`;
+  });
   res.send(`
-    <h2>hello ${req.query.name}</h2>
+    <form method="post" action="/add">
+      <input type="text" name="task" id="task" placeholder="Your new task." />
+      <input type="submit" value="Add" id="submit" />
+    </form>
     <p>your tasks</p>
     <ul>
-     <li>learn html </li>
+     ${taskList}
     </ul>
     `);
+});
+
+app.get("/:index", (req, res) => {
+  const index = req.params.index;
+  res.send(todos[index]);
+});
+
+app.post("/add", (req, res) => {
+  todos.push(req.body.task);
+  res.redirect("/");
+});
+
+app.post("/delete/:index", (req, res) => {
+  const index = req.params.index;
+  todos.splice(index, 1);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
